042184dd19b98dc7fd05ad96c45226c6ce45236c
WBCOHBO <andy405641@gmail.com>
2017-10-16 23:34:38 +0800

    modal-2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { PropTypes } from "react";
-import { Alert ,TouchableOpacity,SectionList,Text,StyleSheet, View, ListView, TouchableHighlight, Dimensions, Image, Animated, TextInput,ViewPropTypes} from "react-native";
+import { Dimensions ,TouchableOpacity,SectionList,Text,StyleSheet, View, ListView, TouchableHighlight, Image, Animated, TextInput,ViewPropTypes} from "react-native";
 import Button from 'react-native-button';
 import { Actions } from 'react-native-router-flux';
 import TeamData from '../components/TeamData';
+import Modal from 'react-native-modalbox';
 
 const {width, height} = Dimensions.get('window')
 
@@ -66,8 +67,23 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     marginRight: 5
   },
-  alertBox:{
-    backgroundColor:'red'
+  modal3:{
+    height: 300,
+    width: 300
+  },
+  btn: {
+    margin: 10,
+    backgroundColor: "#3B5998",
+    color: "white",
+    padding: 10,    
+  },
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  PopText:{
+    color: "black",
+    fontSize: 20,
   },
 });
 
@@ -76,8 +92,11 @@ export default class extends React.Component {
     super(props)
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
     this.state = {
-        dataSource: ds.cloneWithRows(TeamData),
-    }
+      dataSource: ds.cloneWithRows(TeamData),
+      isOpen: false,
+      isDisabled: false,
+      swipeToClose: true,
+    };
 }
 
 // ListView
@@ -86,31 +105,31 @@ renderRow(rowData){
   return (
       <View style={styles.containerCell}>
               <View style={styles.footerContainer}>
-                  <Button>
+                  <View>
+                  <Button onPress={() => this.refs.modal3.open()} style={styles.btn}>
                       <Image 
                           style={styles.imageAvatar}
                           source={{uri: rowData.user}}
                       />
-                  </Button>
+                    </Button>
+                  </View>
                     <View>
                       <Text style={[styles.personal]}>{rowData.personal}</Text>
                       <Text style={styles.personal}>{rowData.personalID}</Text>
                       <Image style={styles.personalPhoto}
                       source={{uri: rowData.personalPhoto}}/>
                     </View>
+
                     <Text style={styles.textSection}>{rowData.section}</Text>
                     <Text style={[styles.textSection]}>{rowData.section2}</Text>
-                  <Button style={styles.alertBox} onPress={() => Alert.alert('測試','9487',
-                    [
-                      {text:"選項1",onPress:Actions.searchFriend},
-                      {text:"選項2",onPress:Actions.searchFriend,style: 'cancel'}
-                    ]
-                  )}>
+                    <Button onPress={() => this.refs.modal3.open()} style={styles.btn}>
                     <Text style={[styles.Teamtext]}>{rowData.team}</Text>
-                    <Text style={[styles.text]}>{rowData.friend}</Text>
+                    <Text style={[styles.text]}>{rowData.friend}</Text>               
                     </Button>
+
               </View>
       </View>
+      
     )
 }
 
@@ -129,9 +148,17 @@ render(){
                 style={styles.listContainer}
                 renderRow={this.renderRow.bind(this)}
                 dataSource={this.state.dataSource}
-              />                 
+              />      
+              
+
+              <Modal style={[styles.modal, styles.modal3]} position={"center"} ref={"modal3"} >
+                  <Text style={styles.PopText}>我有一隻小毛驢</Text>
+                  <Button onPress={Actions.searchFriend} style={styles.btn}>test1 </Button>
+                  <Button onPress={Actions.searchFriend} style={styles.btn}>test2 </Button>
+              </Modal>     
             </Animated.View>                        
         </View>
+        
     )
 }
 }
