985281b84b9c8b477c7408e5b21797154c275640
nightjaye <jeffs850513@gmail.com>
2017-11-29 23:20:22 +0800

    更改Firebase為共用帳號

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyBG6QuNyTDli9kdlKwDOng5mxb9sH_hT5w",
-    authDomain: "gps-f0e10.firebaseapp.com",
-    databaseURL: "https://gps-f0e10.firebaseio.com",
-    projectId: "gps-f0e10",
-    storageBucket: "gps-f0e10.appspot.com",
-    messagingSenderId: "164901076530"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyDNCOCFYGBWsjFIOyugwMmiA66Vj0cQqlU",
+  authDomain: "gpsreactnative.firebaseapp.com",
+  databaseURL: "https://gpsreactnative.firebaseio.com",
+  projectId: "gpsreactnative",
+  storageBucket: "gpsreactnative.appspot.com",
+  messagingSenderId: "689415095077"
+};
+firebase.initializeApp(config);
 export default firebase;
